9cf6ff0bc6f753e0451a9dde2bd8cee7e26999a7
lavanya <lavanya.tatiparthi97@gmail.com>
2023-11-07 21:31:55 +0530

    fourth commit

--- App.js
+++ App.js
@@ -27,22 +27,22 @@ root.render(heading);
 // root.render(parent);
 
 
-// const parent = React.createElement('div',{},[
-//   React.createElement('div',{id:'child'},[
-// React.createElement('div',{className:'header'},'This is Main Heading'),
-// React.createElement('h1',{},'This is first element'),
-// React.createElement('h2',{},'This issecond element'),
-// React.createElement('h3',{},'This is third element'),
-//   ]),
-//   React.createElement('div',{id:'child2'},[
-//     React.createElement('h1',{},'This is first element'),
-// React.createElement('h2',{},'This is second element'),
+const parent = React.createElement('div',{},[
+  React.createElement('div',{id:'child'},[
+React.createElement('div',{className:'header'},'This is Main Heading'),
+React.createElement('h1',{},'This is first element'),
+React.createElement('h2',{},'This issecond element'),
+React.createElement('h3',{},'This is third element'),
+  ]),
+  React.createElement('div',{id:'child2'},[
+    React.createElement('h1',{},'This is first element'),
+React.createElement('h2',{},'This is second element'),
 
-//   ])
-// ]);
+  ])
+]);
 
-// const root1 = ReactDOM.createRoot(document.getElementById("root1"));
-// root1.render(parent);
+const root1 = ReactDOM.createRoot(document.getElementById("root1"));
+root1.render(parent);
 
 
 
